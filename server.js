adf1796c1cadea2a503ba6dc491af3ecc977c827
Lessa Winston <lessa.winston@protonmail.com>
2021-07-01 21:21:27 +0000

    completed post to /api/users step and passed test

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+var usersList = [];
 
-app.use(cors())
-app.use(express.static('public'))
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.static('public'));
+app.use('/api/users', bodyParser.urlencoded({extended: false}));
+
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
+app.post('/api/users', function (req, res) {
+  if (!usersList.includes(req.body.username)) {
+    usersList.push(req.body.username);
+  };
+  
+  res.json({username: req.body.username, _id: usersList.indexOf(req.body.username)});
 
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
